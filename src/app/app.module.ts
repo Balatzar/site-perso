97ba3ebd88afd0d3f2cbbc02433afc8dd64fcaeb
balatzar <bataprod@gmail.com>
2016-12-12 21:20:32 +0100

    add testo again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestoComponent } from './testo/testo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TestoComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/testo/testo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testo',
+  templateUrl: './testo.component.html',
+  styleUrls: ['./testo.component.sass']
+})
+export class TestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
